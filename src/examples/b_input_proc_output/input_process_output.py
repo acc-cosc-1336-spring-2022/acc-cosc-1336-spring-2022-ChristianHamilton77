6f544d02c896203fe37d00b19a1fe604ce4d8faa
ChristianHamilton77git config --global user.email christian.hamilton@g.austincc.edugit config --global user.name ChristianHamilton77git config --global user.name ChristianHamilton77 <christian.hamilton@g.austincc.edu>
2022-02-05 13:05:45 -0600

    commit

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,11 @@ def display_output():
 def test_config():
     return True
 
+def add_numbers(num1,num2):
+    return num1 + num2
+
+def multiply_numbers(num1,num2):
+    return num1 * num2
+
+def acres(sqft):
+    return sqft / 43560
--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,3 +1,7 @@
 import input_process_output
 
 input_process_output.display_output()
+num1 = int(input('enter a number for 1st number '))
+num2 = int(input('enter a number for 2nd number '))
+
+print(input_process_output.add_numbers(num1,num2))
