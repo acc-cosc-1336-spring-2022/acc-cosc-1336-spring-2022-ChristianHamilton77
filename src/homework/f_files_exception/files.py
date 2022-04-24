9fcf144bfcf8a9cd51310a01dd4cb9e4bda5f7ff
ChristianHamilton77git config --global user.email christian.hamilton@g.austincc.edugit config --global user.name ChristianHamilton77git config --global user.name ChristianHamilton77 <christian.hamilton@g.austincc.edu>
2022-04-23 21:11:56 -0500

    Homework 8

--- src/homework/f_files_exception/files.py
+++ src/homework/f_files_exception/files.py
@@ -1,1 +1,69 @@
 #
+import json
+
+def writeFile(textToAdd,method):
+    dataFile = open('data.txt',method) #open data File in Write or append mode Mode
+    for key, value in textToAdd.items():#add the text
+        dataFile.write(key + '\n')
+        dataFile.write(str(value) + '\n')
+
+    dataFile.close() #close File
+
+def writeDataFile(textToAdd,method):# method to be "w" or "a"
+    if method == "w" or method == "W":
+        method == "w"
+        writeFile(textToAdd,method)
+    elif method == "a" or method == "A":
+        method == "a"
+        writeFile(textToAdd,method)
+    else:
+        print("method must be a w (write) or a (append)")
+
+def readFile():
+    # dataFile = open('data.txt', 'r') #open data File in read mode
+    # data = dataFile.readlines() #read whole file to a string
+    # dataFile.close() #close File
+    # print(data)
+
+    dataFile = open('data.txt', 'r') 
+    dataFileItem = dataFile.readline()
+    while dataFileItem != '': # read and display lines
+        DataFileItemValue = dataFile.readline()
+        dataFileItem = dataFile.readline()
+        dict += "dataFileItem.rstrip('\n'):DataFileItemValue.rstrip('\n')"
+    dataFile.close
+
+# test = "newsfeed"
+# writeDataFile(test,"a")
+# readFile()
+
+def add_inventory(widgets,widget_name,quantity):
+    #print('')
+    quantity = int(quantity)#make input an int
+    
+    if widget_name in widgets:
+        widgets[widget_name] += quantity
+    else:
+        widgets[widget_name] = quantity
+    writeDataFile(widgets,"w")
+    return widgets
+def remove_inventory_widget(widgets,widget_name):
+    # print('')
+    if widget_name in widgets:
+        # print('Dictionary Widget found.')
+        del widgets[widget_name]
+    writeDataFile(widgets,"w")
+    # else:
+    #     print('Sorry the Widget Name was not found in the Dictionary.')
+
+# dict = {"newsfeed":1,"recentOrder":4}
+# print(dict,"/n")
+
+# add_inventory(dict,"newProduct",3)
+# print(dict,"/n")
+
+# remove_inventory_widget(dict,"newProduct")
+# print(dict,"/n")
+
+# remove_inventory_widget(dict,"newProduct")
+# print(dict,"/n")  
--- src/homework/f_files_exception/main.py
+++ src/homework/f_files_exception/main.py
@@ -1,1 +1,35 @@
 #
+import files
+
+menu = "\nPlease make a selection:\n1-Add or Update Item\n2-Delete Item\n3-Exit\n"
+# print(menu)
+
+dict = {}
+choice = 0
+while choice != 3:
+    try:
+        choice = int(input(menu))
+    except ValueError:
+        choice = 0
+
+    if choice == 1:
+        dataName = input('Enter an item name to add or edit.\n')
+        value = dict.get(dataName)
+        if value:
+            dataValue = input(dataName + ' is in the dictionary, enter the number value to add to ' + dataName + '.\n')
+            files.add_inventory(dict,dataName,dataValue)
+            print('\n' + dataName + ':' + dataValue + ' has been added to the dictionary.\n')
+            #print(dict)
+        else:
+            dataValue = input(dataName + ' was not found in the dictionary, enter the number value to add to ' + dataName + '.\n')
+            files.add_inventory(dict,dataName,dataValue)
+            print('\n' + dataName + ':' + dataValue + ' has been added to the dictionary.\n')
+           # print(dict)
+    if choice == 2:
+        dataName = input('Enter an item name to delete.\n')
+        value = dict.get(dataName)
+        if value:
+            files.remove_inventory_widget(dict,dataName)
+            print('\n' + dataName + ' has been removed from the dictionary.\n')
+        else:
+            print(dataName + ' was not found in the dictionary.\n')
